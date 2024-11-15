7fdfb96e932c29795410d1c667b360ff26b0d92b
matozinho <matozinhof.academico@gmail.com>
2024-11-14 23:58:35 -0300

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+	return (
+		<div className="flex flex-col gap-4 items-center justify-center">
+			<p className="font-bold">IonicIdioms</p>
+			<Button>Button test</Button>
+		</div>
+	);
+}
+
+export default App;
